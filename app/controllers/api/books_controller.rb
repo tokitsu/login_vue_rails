4855fdf7d53cdefaf142bf099dbd0f81c7d836a4
tokitsu <toki2222son@yahoo.co.jp>
2021-06-27 11:51:07 +0900

    add create and update

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,4 +1,6 @@
 class Api::BooksController < ApplicationController
+  protect_from_forgery :except => [:create, :update]
+
   def show
     @book = Book.find(params[:id])
     render 'show', formats: 'json', handlers: 'jbuilder'
@@ -7,5 +9,31 @@ class Api::BooksController < ApplicationController
   def index
     @books = Book.all
     render 'index', formats: 'json', handlers: 'jbuilder'
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      head :no_content
+    else
+      render json: @book.errors, status: :unprocessable_entry
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      head :no_content
+    else
+      render json: @book.errors, status: :unprocessable_entry
+    end
+  end
+
+  private
+
+  def book_params
+    params.fetch(:book, {}).permit(
+      :title, :author, :publisher, :genre
+    )
   end
 end
--- app/javascript/router/router.js
+++ app/javascript/router/router.js
@@ -1,11 +1,15 @@
 import Vue          from 'vue'
 import VueRouter    from 'vue-router'
 import BookHome     from '../pages/BookHome.vue'
+import BookCreate   from '../pages/BookCreate.vue'
+import BookUpdate    from '../pages/BookUpdate.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', name: 'BookHome', component: BookHome },
+  { path: '/',             name: 'BookHome',   component: BookHome },
+  { path: '/create',       name: 'BookCreate', component: BookCreate},
+  { path: '/update/:id',   name: 'BookUpdate',   component: BookUpdate},
 ];
 
 export default new VueRouter({ routes });
